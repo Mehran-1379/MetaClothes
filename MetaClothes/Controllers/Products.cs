ed0feadbefef5fa05cfaa7f6fa8a9c1be7d31b50
Mehran-1379 <121351077+Mehran-1379@users.noreply.github.com>
2023-12-29 22:49:29 +0330

    Controller

---
+++ MetaClothes/Controllers/Products.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetaClothes.Controllers
+{
+    [ApiController]
+    public class Products: ControllerBase
+    {
+        public JsonResult GetProducts()
+        {
+           return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, name = "Shirt"},
+                    new {id = 2, name = "Shoe"}
+                }
+                );
+        }
+    }
+}
