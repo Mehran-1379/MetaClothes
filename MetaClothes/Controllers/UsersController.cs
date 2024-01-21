f90704224b81f5335573cf9981334f54a3542868
Mehran-1379 <121351077+Mehran-1379@users.noreply.github.com>
2024-01-22 00:26:08 +0330

    Hashing

---
+++ MetaClothes/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MetaClothes.Models;
+using Npgsql;
+using MetaClothes.Tools;
+namespace MetaClothes.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly string connectionString = "Port=1379;Host=localhost" +
+            ";Database=MetaClothes;Username=postgres;" +
+            "Persist Security Info=True;Password=13ColonelRhodes79";
+
+        //public UsersController(string connectionString)
+        //{
+        //    this.connectionString = connectionString;
+        //}
+
+        [HttpPost]
+        [Route("login")]
+
+        public async Task<IActionResult> userLogin([FromBody] CustomerDTO user)
+        {
+
+            String password = Password.hashpassword(user.Password);
+            await using var connection = new NpgsqlConnection(connectionString);
+            await connection.OpenAsync();
+            var sql = "SELECT * FROM customers WHERE email = @email AND password = @password";
+            await using var cmd = new NpgsqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@email", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+
+            if(cmd == null)
+            {
+                return BadRequest("This Username or password is incorrect!");
+            }
+
+
+            return Ok(cmd);
+
+        }
+
+    }
+}
---
+++ MetaClothes/Tools/Password.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MetaClothes.Tools
+{
+    public class Password
+    {
+
+        public static string hashpassword(string password)
+        {
+
+            var sha = SHA256.Create();
+            var asByteArray = Encoding.Default.GetBytes(password);
+            var hashedpassword = sha.ComputeHash(asByteArray);
+            return Convert.ToBase64String(hashedpassword);
+
+
+
+        }
+
+
+
+    }
+}
